9624bef59b34c39b87dc58547faa9284b5c04add
Nolan Stucky <nolan@stucky.net>
2021-02-15 15:16:29 -0800

    save button now saves description to local storage

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $(".saveBtn").on("click", function(){
-    console.log($(this).attr("id"))
+    let hour = $(this).attr("id");
+    let description = $(this).siblings(".description").val();
+    // console.log(description)
+    // console.log(hour);
+    localStorage.setItem(hour, description);
 })
